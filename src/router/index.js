4b38a7aa01cd8b10d7091304df7fe2184b51a12b
uncoder <huiqiang.shi@wenba100.com>
2016-11-30 16:55:55 +0800

    修改

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {Router, Route, Link,browserHistory} from 'react-router';
+import {Router, Route, Link,hashHistory,browserHistory} from 'react-router';
 
 import Index from '../component/index.jsx';
 import Section from '../component/section.jsx';
 
 let router = (
-		<Router history={browserHistory} >
+		<Router history={hashHistory} >
 			<Route path='/' component={Index} >
 				<Route path='/section' component={Section} />
 			</Route>
